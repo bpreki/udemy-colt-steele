189cbffd56aead2a8858844ff0116dcaa09cbe9d
Byron Prescott <prescott.byron@gmail.com>
2018-08-01 04:59:44 +0100

    color game with random colours easy and hard mode all elements styled

--- javascript/dom/colorgame/script.js
+++ javascript/dom/colorgame/script.js
@@ -1,25 +1,88 @@
-var colors=[
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-]
+var numsqr = 6;
+var colors = [];
+var pickedColor;
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
 var clrGl = document.getElementById("clrDis");
+var mode =document.querySelectorAll(".mode");
+var rst = document.querySelector("#reset")
+var easy = document.querySelector("#easy")
+var hard = document.querySelector("#hard")
+var msg= document.querySelector("#msg");
+var h1 = document.querySelector('h1');
 
+init();
+
+function init(){
+  //mode button event listeners
+  for(var i = 0; i< mode.length; i++){
+    mode[i].addEventListener("click", function(){
+      mode[0].classList.remove("selected");
+      mode[1].classList.remove("selected");
+      this.classList.add("selected");
+      this.textContent === "easy" ? numsqr = 3: numsqr = 6;
+      reset();
+    });
+  }
+  for(i=0;i<squares.length;i++){
+    squares[i].addEventListener("click", function(){
+      var clickedColor = this.style.backgroundColor;
+      if(clickedColor === pickedColor){
+        msg.textContent="!!! winner !!!";
+        rst.textContent = "Play again?";
+        changeColors(clickedColor);
+        h1.style.backgroundColor = pickedColor;
+        msg.style.color = pickedColor;
+      }else{
+        this.style.backgroundColor = "#252525";
+        msg.style.color = clickedColor;
+        msg.textContent = "!! Try again !!";
+      }
+    });
+  }
+  reset();
+}
+function reset(){
+  colors= gnrndclr(numsqr);
+  pickedColor = pickcolor();
   clrGl.textContent = pickedColor;
-
-for(i=0;i<squares.length;i++){
-  squares[i].style.backgroundColor = colors[i];
-  squares[i].addEventListener("click", function(){
-    var clickedColor = this.style.backgroundColor;
-    if(clickedColor === pickedColor){
-alert("winner")
+  h1.style.backgroundColor = "steelblue";
+  rst.textContent = "New colour's";
+  msg.textContent = "";
+  for(var i = 0; i < squares.length; i++){
+    if(colors[i]){
+      squares[i].style.display = "block";
+      squares[i].style.backgroundColor = colors[i];
     }else{
-      this.style.backgroundColor = "rgb(35, 35, 35)";
+      squares[i].style.display = "none";
     }
-  })
+  }
+}
+rst.addEventListener("click", function(){
+  reset();
+})
+
+function changeColors(color){
+  for(var i=0;i<squares.length;i++){
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickcolor(){
+  var rndNum =  Math.floor(Math.random()*colors.length);
+  return colors[rndNum];
+}
+
+function gnrndclr(num){
+  var arr = []
+  for(var i = 0 ; i<num; i++){
+    arr.push(rndclr())
+  }
+  return arr;
+}
+
+function rndclr(){
+  var r =Math.floor(Math.random()*256);
+  var g =Math.floor(Math.random()*256);
+  var b =Math.floor(Math.random()*256);
+  return "rgb("+r+", "+g+", "+b+")";
 }
