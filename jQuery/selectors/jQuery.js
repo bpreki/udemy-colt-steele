ccfe6928d71bf011a92ecd286af9f77f80d746e4
Byron Prescott <prescott.byron@gmail.com>
2018-08-01 12:36:24 +0100

    First jQuery excerise with bonus challenge

---
+++ jQuery/selectors/jQuery.js
@@ -0,0 +1,8 @@
+//select all divs apply purple background
+$("div").css('background-color', 'purple');
+//select class="highlight" apply 200px width
+$(".highlight").css("width",'200px');
+// select by id apply orange border
+$("#third").css("border", "2px solid orange");
+// ***BONUS*** select div one apply text color pink ***bonus***
+$("div:contains('1')").css("color", "pink");
