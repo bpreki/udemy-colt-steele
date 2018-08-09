0373616882ee3f45b8940fb06cdc43218975d228
Byron Prescott <prescott.byron@gmail.com>
2018-08-09 14:33:37 +0100

    added remove and fade events on elements added notation for referance

--- todoList/assets/js/todos.js
+++ todoList/assets/js/todos.js
@@ -1,3 +1,13 @@
 $("li").click(function(){
   $(this).toggleClass("completed")
-})
+});
+//click X to delete item
+$("span").click(function(event){
+  //selects li element
+  $(this).parent().fadeOut(750, function(){
+// removes item after fadeout completes
+    $(this).remove()
+  });
+  //prevents parent events firing when span is selected 
+  event.stopPropagation();
+});
