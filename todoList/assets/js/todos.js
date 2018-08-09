abc1cd8fc3dd0dc5e54ca1f9cd3540ac04a6a3a3
Byron Prescott <prescott.byron@gmail.com>
2018-08-09 15:11:52 +0100

    functionality completed

--- todoList/assets/js/todos.js
+++ todoList/assets/js/todos.js
@@ -1,13 +1,28 @@
-$("li").click(function(){
+
+// cross off items on todo list
+$("ul").on("click","li", function(){
+  //toggles class attributes checked or unchecked
   $(this).toggleClass("completed")
 });
+
+
 //click X to delete item
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
   //selects li element
   $(this).parent().fadeOut(750, function(){
 // removes item after fadeout completes
     $(this).remove()
   });
-  //prevents parent events firing when span is selected 
+  //prevents parent events firing when span is selected
   event.stopPropagation();
 });
+
+//add new todos to the list
+$("input[type ='text']").keypress(function(e){
+  if(e.which === 13){
+    //selects text from input
+    var todo = $(this).val();
+    //creates new todo element
+    $("ul").append("<li><span>x</span> " +todo+"</li>")
+  }
+})
