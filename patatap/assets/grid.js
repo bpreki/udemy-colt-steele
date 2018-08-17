69fd54a75a089fcf0739ad7ce59aa3c0fe913670
Byron Prescott <prescott.byron@gmail.com>
2018-08-17 15:30:01 +0100

    placed new circles into array to perform functions on

--- patatap/assets/grid.js
+++ patatap/assets/grid.js
@@ -1,5 +1,18 @@
-var path = new Path();
-var myCircle = new Path.Circle(new Point(0, 0), 50);
-myCircle.fillColor = "red";
+var circles = []
+function onKeyDown (event) {
+  var maxPoint = new Point(view.size.width, view.size.height)
+  var randomPoint = Point.random()
+  var point = maxPoint * randomPoint
+  var 
+  circles.push(new Path.Circle(point, 10).fillColor = 'red')
+}
+var animatedCircle = new Path.Circle(new Point(300, 300), 100);
+animatedCircle.fillColor = 'blue';
 
-myCircle((20,20) 20);
+function onFrame (event) {
+  for (var i = 0; i < circles.length; i++) {
+    circles[i].fillColor.hue += 1
+  }
+  animatedCircle.fillColor.hue += 1
+  animatedCircle.scale(0.9)
+}
